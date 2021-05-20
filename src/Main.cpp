5d455ce2c41d6c1d841d710771a5c7c90cd21563
Sensxy <70344661+Sensxy@users.noreply.github.com>
2021-05-20 15:05:27 -0600

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,50 @@
+#include "GamePlay.h"
+
 int main()
 {
     GamePlay game;
 
-// Want user to be able to start game, look at score, exit, etc
     int choice;
+    do
+    {
+        system("cls");
+        game.showTitle();
+
+        cout<<endl;
+        cout<<"\t1. Play\n";
+        cout<<"\t2. View scores\n";
+        cout<<"\t3. How to play?\n";
+        cout<<"\t4. Exit\n";
+        cout<<"Enter your choice (1/2/3/4): ";
+        cin>>choice;
+
+        if (choice == 1)
+        {
+            game.getPlayerName();
+            char ch;
+            do {
+                game.startGame();
+                cout<<"Want to play again? (y/n): ";
+                cin>>ch;
+            } while (ch == 'y');
+        }
+        else if (choice == 2)
+        {
+            game.showHighScores();
+        }
+        else if (choice == 3)
+        {
+            game.showRules();
+        }
+        else if (choice == 4)
+        {
+            cout<<"Goodbye"<<endl;
+        }
+        else
+        {
+            cout<<"Invalid choice"<<endl;
+        }
+    } while (choice != 4);
+
+    return 0;
+}
